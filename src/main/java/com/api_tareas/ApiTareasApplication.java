c927b4e3e356735c24c5e4146d258fbf992e1752
Pabalberione <pabalberione@gmail.com>
2024-06-20 11:22:48 -0300

    First commit

---
+++ src/main/java/com/api_tareas/ApiTareasApplication.java
@@ -0,0 +1,13 @@
+package com.api_tareas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTareasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTareasApplication.class, args);
+	}
+
+}
